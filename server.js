9168fc9d0f74bbc0a349ce9a25555513449c6067
Dharmit Lakhani <lakhanidharmit@yahoo.com>
2022-08-30 20:58:10 +0530

    admin initialized

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const serverConfig = require('./configs/server.config')
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const dbConfig = require('./configs/db.config');
-
+const bcrypt = require('bcryptjs');
+const User = require('./models/user.model');
+const constants = require('./utils/constants');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
@@ -15,9 +17,19 @@ const db = mongoose.connection;
 db.on("error", ()=>{
     console.log("#### Error while connecting to mongoDB ####");
 });
-db.once("open",()=>{
+db.once("open", async()=>{
+    await User.create(admin);
     console.log("#### Connected to mongoDB ####");
 });
+
+const admin = {
+    name : "Admin",
+    username : "admin",
+    email : "admin@app.com",
+    password : bcrypt.hashSync("AdminOfApp#1", 10),
+    userType : constants.userTypes.admin
+}
+
 
 require('./routes/auth.route')(app);
 require('./routes/report.route')(app);
